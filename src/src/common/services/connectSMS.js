bafd85146a44599c75ebf222f0db605bc99b0ebb
Paulus <pwilhem@mtc.com.na>
2026-06-18 15:15:27 +0200

    Test

--- src/src/common/services/connectSMS.js
+++ src/src/common/services/connectSMS.js
@@ -23,7 +23,7 @@ async function callExternalApi(param1, param4, param5) {
 
   try {
     let response = await sendSms();
-    console.log("sms connect response: ", response)
+    console.log("sms connect response: ", response, param1, param4, param5)
     if (!response.ok) {
 
       const errorText = await response.text();
